65f95180d2795c77cb479d4ca927145be37474cc
Dejiang Chen <dejiang27@gmail.com>
2019-10-07 16:22:49 -0500

    Added article component

--- reddit-clone/src/app/app.component.ts
+++ reddit-clone/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-clone';
+  //title = 'reddit-clone';
+  submitArticle(title:HTMLInputElement, link: HTMLInputElement):boolean{
+    console.log('add title: ${title.value} and link: ${link.value}');
+    return false;
+  }
 }
